6f75228783e37e05c5d59e7affb3dd1325055d27
Atahualpa <netlighter@live.com>
2012-06-14 20:24:53 -0300

    improved organization

--- public/application.js
+++ public/application.js
@@ -1,31 +1,75 @@
-var User = Backbone.Model.extend({});
+﻿(function ($) {
 
-var Users = Backbone.Collection.extend({
- model: User,
-   url: 'http://localhost:9393/fruits'
-});
-var users = new Users;
+    //demo data
+    var users = [
+        { name: "Contact 1", fruit: "apple"},
+        { name: "Contact 2", address: "watermellon"}
+    ];
 
-var UserView = Backbone.View.extend({
+	var User = Backbone.Model.extend({});
 
-  handleNewUser: function(data) {
-    var selectField = $('select[name=newFruit]');
-    users.create({content: selectField.val()});
-    //inputField.val('');
-  }
+	var Users = Backbone.Collection.extend({
+ 		model: User,
+   	url: 'http://localhost:9393/users'
+	});
+	
+	var users = new Users;
 
-  , render: function() {
-    // var data = fruits.map(function(fruit) { return fruit.get('content') + '\n'});
-    // var result = data.reduce(function(memo,str) { return memo + str }, '');
-    // $("#chatHistory").text(result);
-		$(this.el).html();
-    return this;
-  }
+	var UserView = Backbone.View.extend({
+		tagName: "user",
+		className: "user-container",
+		template: _.template($("#userTemplate").html()),
 
-});
+  	render: function () {
+    	this.$el.html(this.template(this.model.toJSON()));
+    	return this;
+  	}
+	});
 
-users.bind('add', function(user) {
-  users.fetch({success: function(){view.render();}});
-});
+	var UsersView = Backbone.View.extend({
+  	el: $("#users"),
 
-var view = new FruitView({el: $('#user')});
+   	initialize: function () {
+   		this.collection = new Users(users);
+    	this.render();
+    	this.$el.find("#filter").append(this.createSelect()); 
+    	//this.on("change:filterType", this.filterByType, this);
+    	//this.collection.on("reset", this.render, this);
+  	 },
+
+   	render: function () {
+    	this.$el.find("user").remove();
+
+   		_.each(this.collection.models, function (item) {
+    		this.renderUser(item);
+    	}, this);
+   	},
+   
+   	renderUser: function (item) {
+   		var userView = new UserView({
+   			model: item
+  		});
+   		this.$el.append(userView.render().el);
+   	},
+
+	 	getTypes: function () {
+   		return _.uniq(this.collection.pluck("fruit"));
+   	},
+
+   	createSelect: function () {
+    	var select = $("<select/>", {
+   			html: "<option value=''>Choose one</option>"
+   		});
+
+   		_.each(this.getTypes(), function (item) {
+    		var option = $("<option/>", {
+    			value: item,
+    			text: item
+    		}).appendTo(select);
+    	});
+
+    	return select;
+   	},
+   });
+
+} (jQuery));
